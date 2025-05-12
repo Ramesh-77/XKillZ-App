6e7e520f0f0a755f5f62e8d9964490aed632b674
Ramesh <12301484@students.koi.edu.au>
2025-05-12 15:34:18 +1000

    myaccount page completed

--- src/screens/AccountScreens/MyAccountScreen.jsx
+++ src/screens/AccountScreens/MyAccountScreen.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity, Pressable } from 'react-native';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import LinearGradient from 'react-native-linear-gradient';
 
 
 const MyAccountScreen = () => {
@@ -12,9 +15,130 @@ const MyAccountScreen = () => {
                     <EvilIcons name="user" size={40} color="#fff" />
                 </TouchableOpacity>
             </View>
-            <ScrollView contentContainerStyle={styles.mainContents}>
-
-            </ScrollView>
+            <View style={styles.mainContents}>
+                <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                    {/* swaps btn */}
+                    <LinearGradient
+                        colors={['green', 'black']}
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 1, y: 0 }}
+                        style={{ borderRadius: 10 }}
+                    >
+                        <Pressable
+                            style={{
+                                flexDirection: "row",
+                                alignItems: "center",
+                                paddingVertical: 10,
+                                paddingHorizontal: 20,
+                                borderRadius: 10,
+                            }}
+                            onPress={() => console.log('Swap button pressed')}
+                        >
+                            <Text style={{ fontSize: 25, fontWeight: "500", color: "#ffffff" }}>Swaps: </Text>
+                            <Text style={{ fontSize: 25, fontWeight: "500", color: "#ffffff" }}>10</Text>
+                        </Pressable>
+                    </LinearGradient>
+                    {/* likes btn */}
+                    <LinearGradient
+                        colors={['#ccc', '#000']}
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 1, y: 0 }}
+                        style={{ borderRadius: 10 }}
+                    >
+                        <Pressable
+                            style={{
+                                flexDirection: "row",
+                                alignItems: "center",
+                                paddingVertical: 10,
+                                paddingHorizontal: 20,
+                                borderRadius: 10,
+                            }}
+                            onPress={() => console.log('Swap button pressed')}
+                        >
+                            <Text style={{ fontSize: 25, fontWeight: "500", color: "#ffffff" }}>Likes: </Text>
+                            <Text style={{ fontSize: 25, fontWeight: "500", color: "#ffffff" }}>15</Text>
+                        </Pressable>
+                    </LinearGradient>
+                </View>
+                {/* subscribe btn */}
+                <LinearGradient
+                    colors={['#09B4E4', '#ababab', '#09B4E4']} // Blue → White → Blue
+                    locations={[0, 0.5, 1]}                    // White appears at the center
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    style={{ borderRadius: 10, marginTop: 20 }}
+                >
+                    <Pressable
+                        style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            paddingVertical: 20,
+                            paddingHorizontal: 20,
+                            borderRadius: 10,
+                        }}
+                        onPress={() => console.log('Swap button pressed')}
+                    >
+                        <Text style={{ fontSize: 25, fontWeight: "500", color: "#ffffff" }}>
+                            Subscribe for all features
+                        </Text>
+                    </Pressable>
+                </LinearGradient>
+                {/* personal details */}
+                <View style={styles.btnContainer}>
+                    <View style={styles.iconBtnBox}>
+                        <AntDesign name="user" size={20} color="#09B4E4" style={{marginEnd: 10}} />
+                        <Pressable>
+                            <Text  style={styles.btn}>Personal Details</Text>
+                        </Pressable>
+                    </View>
+                </View>
+                {/* schedule classess */}
+                <View style={styles.btnContainer}>
+                    <View style={styles.iconBtnBox}>
+                        <AntDesign name="clockcircleo" size={20} color="#09B4E4" style={{marginEnd: 10}}/>
+                        <Pressable>
+                            <Text  style={styles.btn}>Schedule Classes</Text>
+                        </Pressable>
+                    </View>
+                </View>
+                {/* Account settings */}
+                <View style={styles.btnContainer}>
+                    <View style={styles.iconBtnBox}>
+                        <AntDesign name="setting" size={20} color="#09B4E4" style={{marginEnd: 10}} />
+                        <Pressable>
+                            <Text  style={styles.btn}>Account Settings</Text>
+                        </Pressable>
+                    </View>
+                </View>
+                {/* Refer a friend */}
+                <View style={styles.btnContainer}>
+                    <View style={styles.iconBtnBox}>
+                        <AntDesign name="adduser" size={20} color="#09B4E4" style={{marginEnd: 10}} />
+                        <Pressable>
+                            <Text  style={styles.btn}>Refer a Friend</Text>
+                        </Pressable>
+                    </View>
+                </View>
+                {/* help */}
+                <View style={styles.btnContainer}>
+                    <View style={styles.iconBtnBox}>
+                        <Feather name="help-circle" size={20} color="#09B4E4" style={{marginEnd: 10}} />
+                        <Pressable>
+                            <Text  style={styles.btn}>Help</Text>
+                        </Pressable>
+                    </View>
+                </View>
+                {/* sign out */}
+                <View style={styles.btnContainer}>
+                    <View style={styles.iconBtnBox}>
+                        <AntDesign name="logout" size={20} color="#09B4E4" style={{marginEnd: 10}} />
+                        <Pressable>
+                            <Text  style={styles.btn}>Sign Out</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
         </SafeAreaView>
     );
 };
@@ -50,11 +174,31 @@ const styles = StyleSheet.create({
     },
 
     mainContents: {
-        paddingStart: 20,
+        // paddingStart: 20,
+        paddingHorizontal: 30,
         paddingTop: 120,
-        paddingBottom: 20,
-        alignItems: "center",
+        marginVertical: 20,
+        // alignItems: "center",
     },
+    btnContainer: { 
+        marginTop: 20, 
+        borderRadius: 10, 
+        paddingHorizontal: 20, 
+        paddingVertical: 10, 
+        borderWidth: 2, 
+        borderColor: "#09B4E4", 
+    },
+    iconBtnBox: { 
+        flexDirection: "row", 
+        alignItems: "center", 
+        justifyContent: "flex-start", 
+        gap: 10 
+    },
+    btn: { 
+        color: "#09B4E4", 
+        fontSize: 20, 
+        fontWeight: "bold"
+    }
 
 
 });
